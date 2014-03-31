bdb7d625678be0e9390e76ef6db3d57555fd0c81
Jubakuba <Jubakuba@gmail.com>
2014-03-31 11:31:48 -0300

    Framework: disable keyguard widget frame (1/2)
    And another user request.  But...I can understand
    	it now that I see it without.  They actually
    	look quite nice this way.
    
    Change-Id: Ice198f5487fb483924e13683879ec56a2402f38e
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2779,6 +2779,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MODLOCK_ENABLED = "lockscreen_modlock_enabled";
 
         /**
+         * Whether to hide the frame behind lockscreen widgets
+         * @hide
+         */
+        public static final String LOCKSCREEN_WIDGET_FRAME_ENABLED = "lockscreen_widget_frame_enabled";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
